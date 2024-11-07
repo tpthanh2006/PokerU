17f56d07fa1c93f91daecac0aaf3bb5b97497f31
tpthanh2006 <tranpthanh2006@gmail.com>
2024-11-06 19:42:14 -0600

    git commit -m 'test backend and frontend integration 1'

--- our_app/backend_new/create_games/models.py
+++ our_app/backend_new/create_games/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+# Demo for backend & frontend integration:
+class React(models.Model):
+  employee = models.CharField(max_Length=30)
+  department = models.CharField(max_Length=30)
