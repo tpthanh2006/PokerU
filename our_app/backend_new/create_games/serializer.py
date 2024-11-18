5a3484cf6efb5e96ff5d84ef21e47531de6ab5a4
tpthanh2006 <tranpthanh2006@gmail.com>
2024-11-17 19:09:46 -0600

    git commit -m "connect react and django successfully"

--- our_app/backend_new/create_games/serializer.py
+++
@@ -1,8 +0,0 @@
-# Convert complex data to native Python data (which are then easily renderd into JSON)
-from rest_framework import serializers
-from . models import *
-
-class ReactSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = React
-    fields = ['employee', 'department']
